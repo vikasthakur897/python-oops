be660af06095feb04804815bd84bfa081ef0c3f3
VikasThakur <vikasthakur1723@gmail.com>
2025-10-25 21:38:40 +0530

    working in class and methods

--- class-and-object/main.py
+++ class-and-object/main.py
@@ -1,7 +1,8 @@
 #Object
-
 x = 1
 y = 10 # Global variable
+
+print(x + y)
 
 def fuction():
     y = 2
@@ -11,3 +12,19 @@ fuction()
 
 print(type(x))
 print(y) 
+
+
+#Class
+
+class MyClass:
+    x = 5
+    y = 10
+
+    def my_function(self):
+        print("Hello from MyClass")
+        print(self.x)
+
+obj = MyClass()
+obj.my_function()
+print(obj.x)
+print(MyClass.y)   
---
+++ methods/main.py
@@ -0,0 +1,5 @@
+string = 'hello'
+
+x = 1
+print(string.upper()) # Using method of string class
+print(type(string))  # Checking type of variable
