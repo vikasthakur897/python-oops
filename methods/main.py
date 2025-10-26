c18f21f5863c3207ce5b23e9bda716363431cb78
VikasThakur <vikasthakur1723@gmail.com>
2025-10-26 14:17:42 +0530

    define types of methods

--- methods/main.py
+++ methods/main.py
@@ -3,3 +3,34 @@ string = 'hello'
 x = 1
 print(string.upper()) # Using method of string class
 print(type(string))  # Checking type of variable
+
+# Instance Method
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+    def show(self):
+        print("Name:", self.name)
+
+
+# Class Method
+
+class Student:
+    college = "PTU"
+
+    @classmethod
+    def change_college(cls, new_name):
+        cls.college = new_name
+
+
+# Static Method
+
+class Math:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+print(Math.add(5, 3))  # 8
+
+
